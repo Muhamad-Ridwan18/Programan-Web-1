17bcc82496460cf71655319ea3aa2ad77efd2ef2
Muhamad-Ridwan18 <mrdwn1812@gmail.com>
2023-05-19 10:17:50 +0700

    pertemuan 8

--- dasar php/project1.php
+++ dasar php/project1.php
@@ -1,7 +1,16 @@
 <?php
 
- echo "Programan WEB 1". PHP_EOL;
- print "Programan WEB 1". PHP_EOL;
- print "Programan WEB 1";
+    $angka_awal = 10;
 
+    function cek_ganjil_genap($angka){
+        if($angka % 2 == 0){
+            return "genap";
+        }else{
+            return "ganjil";
+        }
+    }
+    
+    $result = cek_ganjil_genap($angka_awal);
+    echo "Angka awal: $angka_awal\n";
+    echo "Angka tersebut adalah: $result";
 ?>
